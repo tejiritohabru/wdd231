90ce437962982e88d3c8ca2ae6d01b8812813029
tejiritohabru <ttohabru@gmail.com>
2026-03-14 11:30:09 +0100

    my week 2

---
+++ chamber/scripts/directory.js
@@ -0,0 +1,75 @@
+const url = "data/members.json";
+const cards = document.querySelector("#members");
+
+async function getMembers() {
+const response = await fetch(url);
+const data = await response.json();
+displayMembers(data);
+}
+
+function displayMembers(members){
+
+members.forEach(member =>{
+
+let card = document.createElement("section");
+
+let name = document.createElement("h3");
+let address = document.createElement("p");
+let phone = document.createElement("p");
+let website = document.createElement("a");
+let img = document.createElement("img");
+
+name.textContent = member.name;
+address.textContent = member.address;
+phone.textContent = member.phone;
+
+website.textContent = "Visit Website";
+website.href = member.website;
+website.target = "_blank";
+
+img.src = `images/${member.image}`;
+img.alt = `${member.name} logo`;
+
+card.appendChild(img);
+card.appendChild(name);
+card.appendChild(address);
+card.appendChild(phone);
+card.appendChild(website);
+
+cards.appendChild(card);
+
+});
+}
+
+getMembers();
+
+
+document.getElementById("year").textContent = new Date().getFullYear();
+document.getElementById("lastModified").textContent = document.lastModified;
+
+
+const gridbutton = document.querySelector("#grid");
+const listbutton = document.querySelector("#list");
+const display = document.querySelector("#members");
+
+
+gridbutton.addEventListener("click", () => {
+cards.classList.add("grid");
+cards.classList.remove("list");
+});
+
+listbutton.addEventListener("click", () => {
+cards.classList.add("list");
+cards.classList.remove("grid");
+});
+
+const menuButton = document.querySelector("#menu");
+const navigation = document.querySelector(".navigation");
+
+menuButton.addEventListener("click", () => {
+navigation.classList.toggle("open");
+});
+
+document.querySelector("#year").textContent = new Date().getFullYear();
+
+document.querySelector("#lastModified").textContent = document.lastModified;
